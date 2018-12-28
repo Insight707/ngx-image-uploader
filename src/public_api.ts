9096867d4b21c53f8992edcfa7dd14b7c0cca23b
Valeriy Soldatov <soldatov@raketa.im>
2018-12-28 17:19:02 +0300

    init

---
+++ src/public_api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of image-uploader
+ */
+
+export * from './lib/image-uploader.service';
+export * from './lib/image-uploader.component';
+export * from './lib/image-uploader.module';
+export * from './lib/interfaces';
+export * from './lib/file-queue-object';
